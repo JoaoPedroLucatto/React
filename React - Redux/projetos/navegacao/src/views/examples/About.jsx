f60c4430aabb635d8b2cfc84f9eef1544135929a
João Pedro A Lucatto <joaopedro.ayruth.lucatto@gmai.com>
2022-04-28 21:44:25 -0300

    Seção 5: React Hooks

---
+++ React - Redux/projetos/navegacao/src/views/examples/About.jsx
@@ -0,0 +1,12 @@
+import './About.css'
+import React from 'react'
+
+const About = props =>(
+    <div className= 'About'>
+        {/* <h1>Componente About</h1>
+        <p>O Nosso sistema foi criado</p> */}
+        <iframe src="https://www.mercadolivre.com.br/?matt_word=Default_URL_MLB&matt_tool=55047108&matt_word=Default_URL_MLB&matt_source=google&matt_campaign_id=10768082097&matt_ad_group_id=109874314007&matt_match_type=&matt_network=g&matt_device=c&matt_creative=454929016309&matt_keyword=&matt_ad_position=&matt_ad_type=&matt_merchant_id=&matt_product_id=&matt_product_partition_id=&matt_target_id=aud-873863174345:dsa-1636072209482&gclid=Cj0KCQjw06OTBhC_ARIsAAU1yOUtxjRpKuYAKNKktVt6pOW-XaPXWtHTenNdFIY3-mkZ4LQzQphToJAaAnarEALw_wcB" frameborder="0"></iframe>
+    </div>
+)
+
+export default About
